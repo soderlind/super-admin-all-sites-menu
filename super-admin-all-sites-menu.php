88825010eb6eebd48668d2c2466b0be2f56cf3df
Per Søderlind <per@soderlind.no>
2021-07-30 17:24:50 +0200

    1.1.2

--- super-admin-all-sites-menu.php
+++ super-admin-all-sites-menu.php
@@ -12,7 +12,7 @@
  * Plugin URI: https://github.com/soderlind/super-admin-all-sites-menu
  * GitHub Plugin URI: https://github.com/soderlind/super-admin-all-sites-menu
  * Description: For the super admin, replace WP Admin Bar My Sites menu with an All Sites menu.
- * Version:     1.1.1
+ * Version:     1.1.2
  * Author:      Per Soderlind
  * Network:     true
  * Author URI:  https://soderlind.no
