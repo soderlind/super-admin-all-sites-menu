e0c10d4c823b09328c05216013cb6db43fe9fc54
Per Søderlind <per@soderlind.no>
2021-10-06 20:56:18 +0200

    Cleanup when uninstalling.

---
+++ include/modules/uninstall.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * @link       https://github.com/soderlind/super-admin-all-sites-menu
+ * @since      1.4.5
+ * @package    Soderlind\Multisite
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+delete_site_option( 'allsitemenutimestamp' );
+
