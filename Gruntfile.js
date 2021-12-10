d840094cb25da10caf2a0b5ad6b01f91c64d5e40
Per Søderlind <per@soderlind.no>
2021-12-10 10:04:38 +0100

    Add patch version number option

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,14 +30,15 @@ module.exports = function (grunt) {
 
   grunt.registerTask("syntax", "default task description", function () {
     console.log(
-      "Syntax:\n" +
+      "Update version number:\n" +
+        "\tgrunt patch (update patch version in pacjage.json, composer.json, plugin and readme.txt)\n" +
         "\tgrunt version (read version from pacjage.json and update version in plugin and readme.txt)\n" +
         "\tgrunt version:project:patch  (update patch version in project.json)\n" +
-        "\tgrunt version:patch  (update patch version in project.json, plugin and readme.txt)\n" +
         "\tgrunt version:minor  (update minor version in project.json, plugin and readme.txt)\n" +
         "\tgrunt version:major  (update major version in project.json, plugin and readme.txt)\n"
     );
   });
 
   grunt.registerTask("default", ["syntax"]);
+  grunt.registerTask("patch", ["version:project:patch", "version"]);
 };
