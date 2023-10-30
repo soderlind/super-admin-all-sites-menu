fa47c18a8b507455b8f35d67bf060e590fe68d38
Per Søderlind <per@soderlind.no>
2023-10-30 11:57:45 +0100

    Add build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-i18n'), 'version' => '57724ac235fe76ed1024');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-i18n'), 'version' => '0dfd74642a3992cbba1f');
--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * super-admin-all-sites-menu
- * version: 1.6.4
+ * version: 1.6.5
  * address: https://github.com/soderlind/super-admin-all-sites-menu#readme
  * author:  Per Søderlind
  * license: GPLv2
