3aaaa4cf727164a052b591c5eefcab9ba1c8f31b
Per Søderlind <per@soderlind.no>
2023-10-30 12:03:44 +0100

    Update dependencies and rebuild

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-i18n'), 'version' => '0dfd74642a3992cbba1f');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-i18n'), 'version' => 'd20e45b0d6b8d836e534');
